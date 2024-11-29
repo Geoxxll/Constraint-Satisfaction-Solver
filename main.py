fc49fe5c6628b60713a231c5c66195bcc8796960
Geoxxll <lxiao4@ualberta.ca>
2024-11-29 13:17:45 -0700

    Test with both MRV and FA algorithms --> passed

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import matplotlib.pyplot as plt
 import numpy as np
+import time 
 
 class PlotResults:
     """
@@ -401,93 +402,56 @@ class Backtracking:
 # file = open('tutorial_problem.txt', 'r')
 file = open('top95.txt', 'r')
 problems = file.readlines()
-
+backtracking = Backtracking()
+mrv = MRV()
+fa = FirstAvailable()
+running_time_mrv = []
+running_time_first_available = []
+plotter = PlotResults()
+all_sovled = True
 for p in problems:
+    start_time = time.time()
     # Read problem from string
     g = Grid()
     g.read_file(p)
+    mrv_g = g.copy()
+    fa_g = g.copy()
 
     # Print the grid on the screen
     print('Puzzle')
     g.print()
 
-    # # Print the domains of all variables
-    # print('Domains of Variables')
-    # g.print_domains()
-    # print()
+    print("Trying to solve with mrv first")
+    mrv_g = backtracking.search(mrv_g, mrv)
+    if (mrv_g.is_solved()):
+        print('Solution: ')
+        mrv_g.print()
+        end_time = time.time()
+        running_time_mrv.append(end_time - start_time)
+    else: 
+        print("Sodoku problem is not solved by mrv")
+    print("Now trying to solve with fa")
+    start_time = time.time()
+    fa_g = backtracking.search(fa_g, fa)
+    if (fa_g.is_solved()):
+        print('Solution: ')
+        fa_g.print()
+        end_time = time.time()
+        running_time_first_available.append(end_time - start_time)
+    else:
+        print("Sodoku problem is not solved by fa")
 
-    # # Iterate over domain values
-    # for i in range(g.get_width()):
-    #     for j in range(g.get_width()):
+    print('Is the current grid a solution? ', mrv_g.is_solved() and fa_g.is_solved())
+    if ((not mrv_g.is_solved()) or (not fa_g.is_solved())):
+        all_sovled = False
+        break
+    print('next')
+    print("\n\n\n")
 
-    #         print('Domain of ', i, j, ': ', g.get_cells()[i][j])
-
-    #         for d in g.get_cells()[i][j]:
-    #             print(d, end=' ')
-    #         print()
-
-    # # # Make a copy of a grid
-    # copy_g = g.copy()
-
-    # print('Copy (copy_g): ')
-    # copy_g.print()
-    # print()
-
-    # print('Original (g): ')
-    # g.print()
-    # print()
-
-    # # # Removing 2 from the domain of the variable in the first row and second column
-    # copy_g.get_cells()[0][1] = copy_g.get_cells()[0][1].replace('2', '')
-
-    # # # The domain (0, 1) of copy_g shouldn't have 2 (first list, second element)
-    # print('copy_g')
-    # copy_g.print_domains()
-    # print()
-
-    # # # The domain of variable g shouldn't have changed though
-    # print('g')
-    # g.print_domains()
-    # print()
-
-    # # Instance of AC3 Object
-    # ac3 = AC3()
-
-    # # Making all variables in the first row arc consistent with (0, 0), whose value is 4
-    # variables_assigned, failure = ac3.remove_domain_row(g, 0, 0)
-
-    # # The domain of all variables in the first row must not have 4
-    # print('Removed all 4s from the first row')
-    # g.print_domains()
-
-    # # # variables_assigned contains all variables whose domain reduced to size 1 in the remove_domain_row opeation
-    # print('Variables that were assigned by remove_domain_row: ', variables_assigned)
-
-    # # # failture returns True if any of the variables in the row were reduced to size 0
-    # print('Failure: ', failure)
-    # print()
-
-    # # # Making all variables in the first column arc consistent with (0, 0), whose value is 4
-    # variables_assigned, failure = ac3.remove_domain_column(g, 0, 0)
-
-    # # # The domain of all variables in the first column must not have 4
-    # print('Removed all 4s from the first column')
-    # g.print_domains()
-    # print()
-
-    # # # Making all variables in the first unit arc consistent with (0, 0), whose value is 4
-    # variables_assigned, failure = ac3.remove_domain_unit(g, 0, 0)
-
-    # # # The domain of all variables in the first column must not have 4
-    # print('Removed all 4s from the first unit')
-    # g.print_domains()
-    # print()
-
-    backtracking = Backtracking()
-    mrv = MRV()
-    g = backtracking.search(g, mrv)
-    print('g')
-    g.print()
-    print()
-
-    print('Is the current grid a solution? ', g.is_solved())
+if (all_sovled):
+    print("Top95 sodoku problems solved")
+else:
+    print("Something wrong")
+plotter.plot_results(running_time_mrv, running_time_first_available,
+    "Running Time Backtracking (MRV)",
+    "Running Time Backtracking (FA)", "running_time")
